802a1367af6d3cb9a3913b9dee1b920609aa8013
pith0n <85147481+pi-th0n@users.noreply.github.com>
2021-07-03 10:13:33 -0700

    Updated listShift.py
    Right now, if you enter a shift value that is not an integer, it allows it but the function will say that you have entered something incorrect. By uncommenting (is that a word?) lines 25, 27, and 28, and indenting line 26, it will loop your input till you enter something that is allowed.

--- listShift.py
+++ listShift.py
@@ -1,6 +1,13 @@
+# Right now, if you enter a shift value that is not an integer, it allows it but the function will say that you have entered something incorrect.
+# By uncommenting (is that a word?) lines 25, 27, and 28, and indenting line 26, it will loop your input till you enter something that is allowed.
+
 def listShift(lst, shift): # Simple list shifting program.
-    if shift == 0:
-        return lst
+
+    if isinstance(shift, int): shift = int(shift)
+    else: print("Sorry, it appears you have entered something other than an integer for the value 'shift'.\nPlease try again.")
+
+    if shift == 0: return lst
+
     for _ in range(shift):
         if shift < 0:
             lst.insert(-1, lst[0])
@@ -9,3 +16,13 @@ def listShift(lst, shift): # Simple list shifting program.
             lst.insert(0, lst[-1])
             lst.pop()
     return lst
+
+while True:
+    yourLst = input("Enter a list: ")
+    if isinstance(yourLst, list):
+        break
+
+# while True:
+yourShift = input("Enter an integer: ")
+    # if isinstance(yourShift, int):
+        # break
