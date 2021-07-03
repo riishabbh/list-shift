4525abed20c285b0f9ee720479df559c3052bdf1
pith0n <85147481+pi-th0n@users.noreply.github.com>
2021-07-03 10:29:27 -0700

    Fixed bugs in listShift.py
    If you want to see what happens if the program takes an invalid input, feel free to comment out/delete lines 34 to 43 and replace that with `yourShift = input("Enter an integer: ")`

--- listShift.py
+++ listShift.py
@@ -1,30 +1,45 @@
-# Right now, if you enter a shift value that is not an integer, it allows it but the function will say that you have entered something incorrect.
-# By uncommenting (is that a word?) lines 25, 27, and 28, and indenting line 26, it will loop your input till you enter something that is allowed.
+# If you want to see what happens if the program takes an invalid input,
+# feel free to comment out/delete lines 34 to 43 and replace that with `yourShift = input("Enter an integer: ")`
 
 def listShift(lst, shift): # Simple list shifting program.
 
-    if isinstance(shift, int): shift = int(shift)
-    else: print("Sorry, it appears you have entered something other than an integer for the value 'shift'.\nPlease try again.")
+    isInt = True
+    try:
+        int(shift)
+    except ValueError:
+        isInt = False
+    
+    if not isInt:
+        print("Sorry, it appears you have entered something other than an integer for the value 'shift'.\nPlease try again.")
+    
+    else: # continue
+        shift = int(shift)
 
-    if shift == 0: return lst
+        if shift == 0: return lst
 
-    for _ in range(shift):
-        if shift < 0:
-            lst.insert(-1, lst[0])
-            lst.pop(0)
-        else:
-            lst.insert(0, lst[-1])
-            lst.pop()
-    return lst
+        for _ in range(shift):
+            if shift < 0:
+                lst.insert(-1, lst[0])
+                lst.pop(0)
+            else:
+                lst.insert(0, lst[-1])
+                lst.pop()
+        return lst
 
 while True:
     yourLst = input("Enter a list: ")
     if isinstance(yourLst, list):
         break
 
-# while True:
-yourShift = input("Enter an integer: ")
-    # if isinstance(yourShift, int):
-        # break
+while True:
+    yourShift = input("Enter an integer: ")
+    isYourInt = True
+    try:
+        int(yourShift)
+    except ValueError:
+        isYourInt = False
+    
+    if isYourInt:
+        break
 
 listShift(yourLst, yourShift)
